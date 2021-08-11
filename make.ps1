480125d6632779c6273e75203013abb1195007f8
Anton <noten.b@hotmail.com>
2021-08-12 01:18:50 +0200

    🖥 Automatic make command

---
+++ make.ps1
@@ -0,0 +1,10 @@
+dotnet publish DoodleDigits\DoodleDigits /p:PublishProfile=Runtime
+dotnet publish DoodleDigits\DoodleDigits /p:PublishProfile=Standalone
+
+$Version = (ls .\DoodleDigits\DoodleDigits\bin\publish\standalone\DoodleDigits.exe | % versioninfo).ProductVersion
+
+$PathRuntime = ".\dist\DoodleDigits.{0}.Runtime.Dependent.zip" -f $Version
+$PathStandalone = ".\dist\DoodleDigits.{0}.Standalone.zip" -f $Version
+
+Compress-Archive -Path .\DoodleDigits\DoodleDigits\bin\publish\runtime -DestinationPath $PathRuntime -CompressionLevel Optimal -Force
+Compress-Archive -Path .\DoodleDigits\DoodleDigits\bin\publish\standalone -DestinationPath $PathStandalone -CompressionLevel Optimal -Force
