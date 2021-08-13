d8c54b3f62d2cd5326263202fc0f985d870c2b43
Anton <noten.b@hotmail.com>
2021-08-13 20:13:52 +0200

    ⚙ Installer

--- make.ps1
+++ make.ps1
@@ -8,3 +8,5 @@ $PathStandalone = ".\dist\DoodleDigits.{0}.Standalone.zip" -f $Version
 
 Compress-Archive -Path .\DoodleDigits\DoodleDigits\bin\publish\runtime -DestinationPath $PathRuntime -CompressionLevel Optimal -Force
 Compress-Archive -Path .\DoodleDigits\DoodleDigits\bin\publish\standalone -DestinationPath $PathStandalone -CompressionLevel Optimal -Force
+
+iscc .\Installer\Installer.iss /DVERSION=$Version /O"./dist"
