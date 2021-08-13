cfe3dc81b155d0a5b166302c8d9c6f642aa2c582
Anton <noten.b@hotmail.com>
2021-08-13 17:27:54 +0200

    📐 Trig reciprocals

--- DoodleDigits/DoodleDigits.Core/Execution/Functions/Named/Trigonometry.cs
+++ DoodleDigits/DoodleDigits.Core/Execution/Functions/Named/Trigonometry.cs
@@ -21,82 +21,108 @@ namespace DoodleDigits.Core.Execution {
 
         #endregion
 
-        public static Value Sin(Value value, ExecutionContext<Function> context) {
-            if (value is not IConvertibleToReal convertibleToReal) {
-                return new UndefinedValue();
+        public static Value Sine(Value value, ExecutionContext<Function> context) {
+            if (value is IConvertibleToReal convertibleToReal) {
+
+                RealValue realValue = ConvertArgumentToReal(convertibleToReal, 0, context);
+
+                Rational rational = realValue.Value.Modulus(RationalUtils.Tau);
+
+                // Hardcoded to avoid double-unperfectness
+                if (rational == Rational.Zero) {
+                    return new RealValue(Rational.Zero);
+                }
+
+                if (rational == TauFourth) {
+                    return new RealValue(Rational.One);
+                }
+
+                if (rational == TauHalf) {
+                    return new RealValue(Rational.Zero);
+                }
+
+                if (rational == TauThreeFourths) {
+                    return new RealValue(-Rational.One);
+                }
+
+                return Value.FromDouble(Math.Sin((double) rational));
             }
 
-            RealValue realValue = ConvertArgumentToReal(convertibleToReal, 0, context);
-
-            Rational rational = realValue.Value.Modulus(RationalUtils.Tau);
-
-            // Hardcoded to avoid double-unperfectness
-            if (rational == Rational.Zero) {
-                return new RealValue(Rational.Zero);
-            }
-            if (rational == TauFourth) {
-                return new RealValue(Rational.One);
-            }
-            if (rational == TauHalf) {
-                return new RealValue(Rational.Zero);
-            }
-            if (rational == TauThreeFourths) {
-                return new RealValue(-Rational.One);
-            }
-
-            return Value.FromDouble(Math.Sin((double)rational));
+            return new UndefinedValue();
         }
 
-        public static Value Cos(Value value, ExecutionContext<Function> context) {
-            if (value is not IConvertibleToReal convertibleToReal) {
-                return new UndefinedValue();
+        public static Value Cosine(Value value, ExecutionContext<Function> context) {
+            if (value is IConvertibleToReal convertibleToReal) {
+                RealValue realValue = ConvertArgumentToReal(convertibleToReal, 0, context);
+
+                Rational rational = realValue.Value.Modulus(RationalUtils.Tau);
+
+                // Hardcoded to avoid double-unperfectness
+                if (rational == Rational.Zero) {
+                    return new RealValue(Rational.One);
+                }
+                if (rational == TauFourth) {
+                    return new RealValue(Rational.Zero);
+                }
+                if (rational == TauHalf) {
+                    return new RealValue(-Rational.One);
+                }
+                if (rational == TauThreeFourths) {
+                    return new RealValue(Rational.Zero);
+                }
+
+                return Value.FromDouble(Math.Cos((double)rational));
             }
 
-            RealValue realValue = ConvertArgumentToReal(convertibleToReal, 0, context);
-
-            Rational rational = realValue.Value.Modulus(RationalUtils.Tau);
-
-            // Hardcoded to avoid double-unperfectness
-            if (rational == Rational.Zero) {
-                return new RealValue(Rational.One);
-            }
-            if (rational == TauFourth) {
-                return new RealValue(Rational.Zero);
-            }
-            if (rational == TauHalf) {
-                return new RealValue(-Rational.One);
-            }
-            if (rational == TauThreeFourths) {
-                return new RealValue(Rational.Zero);
-            }
-
-            return Value.FromDouble(Math.Cos((double)rational));
+            return new UndefinedValue();
         }
 
-        public static Value Tan(Value value, ExecutionContext<Function> context) {
-            if (value is not IConvertibleToReal convertibleToReal) {
-                return new UndefinedValue();
+        public static Value Tangent(Value value, ExecutionContext<Function> context) {
+            if (value is IConvertibleToReal convertibleToReal) {
+                RealValue realValue = ConvertArgumentToReal(convertibleToReal, 0, context);
+
+                Rational rational = (realValue.Value + TauFourth).Modulus(RationalUtils.Pi) - TauFourth;
+
+                // Hardcoded to avoid double-unperfectness
+                if (rational == Rational.Zero) {
+                    return new RealValue(Rational.Zero);
+                }
+                if (rational == TauFourth) {
+                    return new UndefinedValue();
+                }
+                if (rational == -TauFourth) {
+                    return new UndefinedValue();
+                }
+
+                return Value.FromDouble(Math.Tan((double)rational));
             }
 
-            RealValue realValue = ConvertArgumentToReal(convertibleToReal, 0, context);
-
-            Rational rational = (realValue.Value + TauFourth).Modulus(RationalUtils.Pi) - TauFourth;
-
-            // Hardcoded to avoid double-unperfectness
-            if (rational == Rational.Zero) {
-                return new RealValue(Rational.Zero);
-            }
-            if (rational == TauFourth) {
-                return new UndefinedValue();
-            }
-            if (rational == -TauFourth) {
-                return new UndefinedValue();
-            }
-
-            return Value.FromDouble(Math.Tan((double)rational));
+            return new UndefinedValue();
         }
 
-        public static Value ArcSin(Value value, ExecutionContext<Function> context) {
+        private static Value TrigReciprocal(Value value, ExecutionContext<Function> context, Func<Value, ExecutionContext<Function>, Value> trigFunction) {
+
+            Value result = trigFunction(value, context);
+
+            if (result is IConvertibleToReal convertibleToReal) {
+                return new RealValue(1 / convertibleToReal.ConvertToReal(context).Value);
+            }
+
+            return new UndefinedValue();
+        }
+
+        public static Value Secant(Value value, ExecutionContext<Function> context) =>
+            TrigReciprocal(value, context, Sine);
+
+        public static Value Cosecant(Value value, ExecutionContext<Function> context) =>
+            TrigReciprocal(value, context, Cosine);
+
+        public static Value Cotangent(Value value, ExecutionContext<Function> context) =>
+            TrigReciprocal(value, context, Tangent);
+
+
+
+        public static Value ArcSine(Value value, ExecutionContext<Function> context) {
             if (value is not IConvertibleToReal convertibleToReal) {
                 return new UndefinedValue();
             }
@@ -106,7 +132,7 @@ namespace DoodleDigits.Core.Execution {
             return Value.FromDouble(Math.Asin((double)realValue.Value));
         }
 
-        public static Value ArcCos(Value value, ExecutionContext<Function> context) {
+        public static Value ArcCosine(Value value, ExecutionContext<Function> context) {
             if (value is not IConvertibleToReal convertibleToReal) {
                 return new UndefinedValue();
             }
@@ -116,7 +142,7 @@ namespace DoodleDigits.Core.Execution {
             return Value.FromDouble(Math.Acos((double)realValue.Value));
         }
 
-        public static Value ArcTan(Value value, ExecutionContext<Function> context) {
+        public static Value ArcTangent(Value value, ExecutionContext<Function> context) {
             if (value is not IConvertibleToReal convertibleToReal) {
                 return new UndefinedValue();
             }
@@ -126,5 +152,31 @@ namespace DoodleDigits.Core.Execution {
             return Value.FromDouble(Math.Atan((double)realValue.Value));
         }
 
+        private static Value TrigArcReciprocal(Value value, ExecutionContext<Function> context, Func<Value, ExecutionContext<Function>, Value> trigArcFunction) {
+            if (value is IConvertibleToReal convertibleToReal) {
+                RealValue real = new RealValue(1 / ConvertArgumentToReal(convertibleToReal, 0, context).Value);
+
+                return trigArcFunction(real, context);
+            }
+
+            return new UndefinedValue();
+        }
+
+        public static Value ArcSecant(Value value, ExecutionContext<Function> context) =>
+            TrigArcReciprocal(value, context, ArcCosine);
+
+        public static Value ArcCosecant(Value value, ExecutionContext<Function> context) =>
+            TrigArcReciprocal(value, context, ArcSine);
+
+        public static Value ArcCotangent(Value value, ExecutionContext<Function> context) {
+            Value result = ArcTangent(value, context);
+            if (result is IConvertibleToReal resultReal) {
+                RealValue realValue = resultReal.ConvertToReal(context);
+                return new RealValue(TauFourth - realValue.Value);
+            }
+        
+
+            return new UndefinedValue();
+        }
     }
 }
