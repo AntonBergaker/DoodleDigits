24c673a0552967ba4a6a698f8824a31a42ef9f4e
Anton Bergåker <noten.b@hotmail.com>
2024-07-28 14:46:17 +0200

    🔼 Update version number

--- DoodleDigits.App/update_dotnet.ps1
+++ DoodleDigits.App/update_dotnet.ps1
@@ -1,5 +1,3 @@
 dotnet publish ../DoodleDigits/DoodleDigits.JsInterop/ -c Release
 
 Copy-Item -Path "../DoodleDigits/DoodleDigits.JsInterop/bin/Release/net8.0/browser-wasm/AppBundle/*" -Destination "./static/doodledigits/" -Recurse -Force
-
-PAUSE
