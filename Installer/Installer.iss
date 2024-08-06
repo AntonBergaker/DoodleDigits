ffc90dae6338cb088d7fa6c1b8144e63769840e5
Anton <noten.b@hotmail.com>
2024-08-06 19:15:38 +0200

    🐧 Linux support (#47)

--- Installer/Installer.iss
+++ Installer/Installer.iss
@@ -27,7 +27,7 @@ ArchitecturesAllowed=x64
 ; done in "64-bit mode" on x64, meaning it should use the native
 ; 64-bit Program Files directory and the 64-bit view of the registry.
 ArchitecturesInstallIn64BitMode=x64
-OutputBaseFilename="DoodleDigits.{#Version}.Installer"
+OutputBaseFilename="DoodleDigits.{#Version}.Windows.Installer"
 
 [Tasks]
 Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; \
--- make.ps1
+++ make.ps1
@@ -3,12 +3,15 @@ New-Item ./dist -ItemType "directory"
 
 cd DoodleDigits.App
 npm run package
+npx electron-forge package --platform linux
 cd ..
 
 $Version = (ls "./DoodleDigits.App/out/Doodle Digits-win32-x64/DoodleDigits.exe" | % versioninfo).ProductVersion
 
-$PathStandalone = ".\dist\DoodleDigits.{0}.Standalone.zip" -f $Version
+$PathStandaloneWin = ".\dist\DoodleDigits.{0}.Windows.Standalone.zip" -f $Version
+$PathStandaloneLinux = ".\dist\DoodleDigits.{0}.Linux.Standalone.zip" -f $Version
 
-Compress-Archive -Path ".\DoodleDigits.App\out\Doodle Digits-win32-x64" -DestinationPath $PathStandalone -CompressionLevel Optimal -Force
+Compress-Archive -Path ".\DoodleDigits.App\out\Doodle Digits-win32-x64" -DestinationPath $PathStandaloneWin -CompressionLevel Optimal -Force
+Compress-Archive -Path ".\DoodleDigits.App\out\Doodle Digits-linux-x64" -DestinationPath $PathStandaloneLinux -CompressionLevel Optimal -Force
 
 iscc .\Installer\Installer.iss /DVERSION=$Version /O"./dist"
